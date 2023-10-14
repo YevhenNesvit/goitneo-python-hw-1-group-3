fbcfe13781c5e1f052e503aacfa7c8feb9bbc901
Yevhen Nesvit <yevhen.nesvit@gmail.com>
2023-10-14 21:39:41 +0300

    add bot module

---
+++ bot.py
@@ -0,0 +1,36 @@
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+def add_contact(args, contacts):
+    if len(args) != 2 :
+        return "Please enter name and phone number!"
+    else :
+        name, phone = args
+
+    if name in contacts.keys() :
+        return f"Contact {name} already exist!"
+    else :
+        contacts[name] = phone
+        return "Contact added."
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
